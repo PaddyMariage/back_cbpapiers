f65bd6e3b5b2e5f772cc601878135da9646afe7e
dri <ab.blackbird@gmail.com>
2020-07-26 23:02:06 +0200

    mysql-version

--- src/main/java/cbpapiers/app/cbpapiers/Security/SecurityConfig.java
+++ src/main/java/cbpapiers/app/cbpapiers/Security/SecurityConfig.java
@@ -11,50 +11,53 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-        @Autowired
-        MyUserDetailsService userDetailsService;
+    @Autowired
+    MyUserDetailsService userDetailsService;
 
-        @Autowired
-        JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
 
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService);
-        }
-
-        //surcharge l'authorization
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            //il faut désactiver le fait de ne pas pouvoir faire des requetes sur un autre serveur
-            http.csrf().disable()
-                    .authorizeRequests()
-                    //pour les requêtes qui seront uniquement accessibles avec un profil admin, il faudra ajouter /admin
-                    .antMatchers("/customers/authentification").permitAll()
-                    //on doit être authentifié pour faire une requête
-                    .anyRequest().authenticated()
-                    .and().exceptionHandling()
-                    .and().sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-            http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        }
-
-
-        @Bean
-        public PasswordEncoder getPasswordEncoder() {
-            return NoOpPasswordEncoder.getInstance();
-        }
-
-        //ajouté pour ne pas avoir de problème avec le autowired de UtilisateurController
-        @Bean
-        @Override
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
-        }
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
+
+    //surcharge l'authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //il faut désactiver le fait de ne pas pouvoir faire des requetes sur un autre serveur
+        http
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and().httpBasic()
+                .and().csrf().disable().authorizeRequests()
+                //pour les requêtes qui seront uniquement accessibles avec un profil admin, il faudra ajouter /admin
+                .antMatchers("/customers/authentification").permitAll()
+                //on doit être authentifié pour faire une requête
+                .anyRequest().authenticated()
+                .and().exceptionHandling()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    //ajouté pour ne pas avoir de problème avec le autowired de UtilisateurController
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
 
